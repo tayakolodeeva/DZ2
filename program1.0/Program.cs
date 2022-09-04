911a5386521ee263f524772c4a726afa06887def
Taya <tayakolodeeva@yandex.ru>
2022-09-04 23:21:40 +0300

    fixprogram

---
+++ program1.0/Program.cs
@@ -0,0 +1,11 @@
+﻿// Нахождение второго по счету числа в трехзначном  
+Console.WriteLine ("Введите 3-х значное число-");
+int number, numberTwo;
+number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number <999)
+{numberTwo = Convert.ToInt32(number/10%10);
+Console.WriteLine($"Второе по счету число {numberTwo}");}
+else
+{
+Console.WriteLine($"Необходимо ввести 3-х значное число"); 
+}
