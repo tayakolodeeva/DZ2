0c8f9fa376bca127d0f68803a007cf0fc714d416
Taya <tayakolodeeva@yandex.ru>
2022-09-06 17:42:54 +0300

    fixprogram2

---
+++ program1.1/Program.cs
@@ -0,0 +1,60 @@
+﻿// Нахождение 3 по счету цифры числа, сообщить если третьей цифры нет.  
+System.Console.WriteLine("Число отрицательное(Да/Нет)?");
+if (Console.ReadLine() == "Нет")
+{
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+{
+Console.Write("Число введенное Вами содержит только 2 цифры, результат - ");
+}
+    else
+    {
+    int c = 1;
+    for (int i = b; i > 3; i--)
+     {
+       c = c * 10;
+     }
+
+        result = (a / c) % 10;
+    }
+return result;
+}}
+else 
+{
+int number = ReadInt("Введите число со знаком - : ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 4)
+    {
+        Console.Write("Число введенное Вами содержит только 2 цифры, результат - ");
+    }
+else
+    {
+    int c = 1;
+    for (int i = b; i > 4; i--)
+    {
+     c = c * 10;
+    }
+
+        result = (a / c) % 10;
+    }
+return result;
+}}
---
+++ program1.2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Нахождение является ли день недели (обозначенный цифрой от 1 до 7) выходным.
+int number = ReadInt("Введите цифру от 1 до 7: ");
+Console.WriteLine(dayOfTheWeek(number));
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+string dayOfTheWeek(int a)
+{
+if (a > 0 || a < 8)
+{
+if (a == 7 || a == 6)
+{
+Console.Write(a+ " - выходной ");
+}
+else
+{
+Console.Write(a+ " - рабочий");
+}
+}
+else
+{
+Console.Write("Введенная цифра должна находиться в диапазоне от 1 до 7");
+}
+return " день.";
+}
