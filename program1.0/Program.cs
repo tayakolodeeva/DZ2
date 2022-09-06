2fd33458c0cb45e763ba2e0259622a783539594a
Taya <tayakolodeeva@yandex.ru>
2022-09-06 17:31:09 +0300

    fixprogram2

--- program1.0/Program.cs
+++ program1.0/Program.cs
@@ -1,11 +1,48 @@
-﻿// Нахождение второго по счету числа в трехзначном  
-Console.WriteLine ("Введите 3-х значное число-");
-int number, numberTwo;
-number = Convert.ToInt32(Console.ReadLine());
-if (number > 99 && number <999)
-{numberTwo = Convert.ToInt32(number/10%10);
-Console.WriteLine($"Второе по счету число {numberTwo}");}
+﻿// Нахождение второго по счету числа в 3-хзначном 
+System.Console.WriteLine("Число отрицательное(Да/Нет)?");
+if (Console.ReadLine() == "Нет") 
+{int number = ReadInt("Введите 3-хзначное число: ");
+int count = number.ToString().Length;
+
+if (count < 3 || count > 3)
+{
+Console.WriteLine("Число введенное Вами не является 3-хзначным");
+}
 else
 {
-Console.WriteLine($"Необходимо ввести 3-х значное число"); 
+Console.WriteLine(InCenter(number));
 }
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}}
+else
+{int number = ReadInt("Введите 3-хзначное число со знаком - : ");
+int count = number.ToString().Length;
+
+if (count < 4 || count > 4)
+{
+Console.WriteLine("Число введенное Вами не является 3-хзначным");
+}
+else
+{
+Console.WriteLine(InCenter(number));
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}}
